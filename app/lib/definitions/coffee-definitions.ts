c640e70ab8d2e04847004167d2c7f0c27fc3a4bb
Piotr Owczarczyk <piotr.owczarczyk98@gmail.com>
2025-01-26 19:59:08 +0100

    Change enums to string unions in coffee definitions

--- app/lib/definitions/coffee-definitions.ts
+++ app/lib/definitions/coffee-definitions.ts
@@ -1,67 +1,82 @@
-enum ProcessingMethod {
-  WASHED = "washed",
-  NATURAL = "natural",
-  WET_HULLED = "wet-hulled",
-  HONEY = "honey",
-  ANAEROBIC = "anaerobic",
-  CARBONIC = "carbonic",
-  PULPED_NATURAL = "pulped natural",
-}
-
-enum RoastLevel {
-  LIGHT = "light",
-  MEDIUM_LIGHT = "medium-light",
-  MEDIUM = "medium",
-  MEDIUM_DARK = "medium-dark",
-  DARK = "dark-roast",
-  ULTRA_DARK = "ultra-dark",
-}
-
-enum Certification {
-  FAIR_TRADE = "fair trade",
-  RAINFOREST_ALLIANCE = "rainforest alliance",
-  ORGANIC = "organic",
-  BIRD_FRIENDLY = "bird-friendly",
-}
-
-enum RoastLevelDetail {
-  CINNAMON = "cinnamon",
-  NEW_ENGLAND = "new england",
-  CITY = "city",
-  FULL_CITY = "full city",
-  VIENNA = "vienna",
-  FRENCH = "french",
-  ITALIAN = "italian",
-  SPANISH = "spanish",
-}
-
-enum BeanType {
-  ARABICA = "arabica",
-  ROBUSTA = "robusta",
-  LIBERICA = "liberica",
-  EXCELSA = "excelsa",
-  ARABICA_ROBUSTA_MIX = "arabica-robusta mix",
-}
-
 export interface SpecialtyCoffee {
   id: string;
   name: string;
-  originType: "single origin" | "blend";
-  singleOriginName?: { country: string; region: string; farmName?: string };
+  description: string;
+  originType: OriginType;
+  singleOriginName?: SingleOriginName;
   beanType: BeanType;
   blendRatio?: string;
   roastingLocation: string;
-  brewMethods: "filter" | "espresso";
+  brewMethod: BrewMethod;
   roastLevel: RoastLevel;
   roastLevelDetail?: RoastLevelDetail;
   processingMethod: ProcessingMethod;
-  elevation: number;
+  /*
+      Elevation in metres.
+      Array of one number gives an approximate elevation.
+      Array of two numbers give a range.
+    */
+  elevation?: number[];
   certifications: Certification[];
   decaf: boolean;
   primaryFlavors: string[];
-  secondaryFlavors: string[];
-  body: "light" | "medium" | "full";
-  acidity: "low" | "medium" | "high";
+  secondaryFlavors?: string[];
+  body: Body;
+  acidity: Acidity;
   priceFor250g: number;
   priceFor1kg: number;
 }
+
+type OriginType = "single origin" | "blend";
+
+type BrewMethod = "filter" | "espresso";
+
+type Body = "light" | "medium" | "full";
+
+type Acidity = "light" | "medium" | "high";
+
+type SingleOriginName = {
+  country: string;
+  region: string;
+  farm?: string;
+};
+
+type ProcessingMethod =
+  | "washed"
+  | "natural"
+  | "wet-hulled"
+  | "honey"
+  | "anaerobic"
+  | "carbonic"
+  | "pulped natural";
+
+type RoastLevel =
+  | "light"
+  | "medium light"
+  | "medium"
+  | "medium dark"
+  | "dark"
+  | "ultra dark";
+
+type Certification =
+  | "fair trade"
+  | "rainforest alliance"
+  | "organic"
+  | "bird-friendly";
+
+type RoastLevelDetail =
+  | "cinnamon"
+  | "new england"
+  | "city"
+  | "full city"
+  | "vienna"
+  | "french"
+  | "italian"
+  | "spanish";
+
+type BeanType =
+  | "arabica"
+  | "robusta"
+  | "liberica"
+  | "excelsa"
+  | "arabica-robusta mix";
