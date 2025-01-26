e00eb8fcdc139922c898f62d05ece60e74846678
Piotr Owczarczyk <piotr.owczarczyk98@gmail.com>
2025-01-26 19:58:11 +0100

    Change enum to string union in accessory definitions

--- app/lib/definitions/accessories-definitions.ts
+++ app/lib/definitions/accessories-definitions.ts
@@ -1,14 +1,13 @@
-enum Category {
-  BREWING_EQUIPMENT = "brewing equipment",
-  FILTERS = "filters",
-  KETTLES = "kettles",
-  GRINDERS = "grinders",
-  SCALES = "scales",
-  COFFEE_STORAGE = "coffee storage",
-  CLEANING_TOOLS = "cleaning tools",
-  CUPS_AND_TUMBLERS = "cups and tumblers",
-  OTHER = "other",
-}
+type Category =
+  | "brewing equipment"
+  | "filters"
+  | "kettles"
+  | "grinders"
+  | "scales"
+  | "coffee storage"
+  | "cleaning tools"
+  | "cups and tumblers"
+  | "other";
 
 export interface Accessory {
   id: string;
