4279bb8e6ebbdd4bceb0222142ac6993d1fc4dc8
Piotr Owczarczyk <piotr.owczarczyk98@gmail.com>
2025-01-27 23:00:23 +0100

    Add the display of dummy filter coffee elements

--- app/shop/filter/page.tsx
+++ app/shop/filter/page.tsx
@@ -1,3 +1,19 @@
+import { fetchAllFilters } from "@/app/lib/actions";
+import { SpecialtyCoffee } from "@/app/lib/definitions/coffee-definitions";
+
 export default function Page() {
-  return <h1>Filter Page</h1>;
+  const fetchedFilterCoffees = fetchAllFilters();
+  const filterCoffees = fetchedFilterCoffees.map((coffee: SpecialtyCoffee) => (
+    <div
+      key={coffee.id}
+      className="border border-black bg-gray-300 w-60 h-80"
+    ></div>
+  ));
+
+  return (
+    <div className="">
+      <h1 className="text-center text-6xl">Filter Coffees</h1>
+      <div className="flex flex-wrap gap-4 justify-center">{filterCoffees}</div>
+    </div>
+  );
 }
