de802a0629e64ed0998831fcd7392766357b62b9
Piotr Owczarczyk <piotr.owczarczyk98@gmail.com>
2025-01-29 21:42:21 +0100

    Add functions to fetch coffees by name

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,13 +1,36 @@
 import { specialtyCoffees } from "@/app/lib/placeholder-data/coffee-placeholder-data";
 import { SpecialtyCoffee } from "@/app/lib/definitions/coffee-definitions";
 import { Accessory } from "@/app/lib/definitions/accessories-definitions";
+import { accessories } from "@/app/lib/placeholder-data/accessories-placeholder-data";
 
-export const fetchAllFilters = (): SpecialtyCoffee[] => {
+export const fetchAllCoffees = (): SpecialtyCoffee[] => {
+  return specialtyCoffees;
+};
+
+export const fetchAllFilterCoffees = (): SpecialtyCoffee[] => {
   return specialtyCoffees.filter((coffee) => coffee.brewMethod === "filter");
 };
 
 export const fetchAllEspressos = (): SpecialtyCoffee[] => {
   return specialtyCoffees.filter((coffee) => coffee.brewMethod === "espresso");
+};
+
+export const fetchCoffeeByName = (coffeeId: string): SpecialtyCoffee | null => {
+  const coffee = specialtyCoffees.find((coffee) => coffee.id === coffeeId);
+  if (!coffee) {
+    return null;
+  }
+  return coffee;
+};
+
+export const fetchAccessoryByName = (accessoryId: string): Accessory | null => {
+  const accessory = accessories.find(
+    (accessory) => accessory.id === accessoryId,
+  );
+  if (!accessory) {
+    return null;
+  }
+  return accessory;
 };
 
 export const fetchAllAccessories = (): Accessory[] => {
